14cbb9a082807729054d75fc5f0171a2daecb4ed
uriell5 <uriell.chila@gmail.com>
2021-01-31 16:47:58 +0000

    aplicación actualizada a v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,8 +5,6 @@ var response;
 
 app.get('/', function (req, res) {
   response = 'This is version 2 of the app.' + '\n';
-
-       //send the response to the client
        res.send(response);
 });
 
